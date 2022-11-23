79e65745c5b133b6927a214f5238203338c29113
Yuliya Kozina <yuliyakc21@gmail.com>
2022-11-23 17:23:26 +0200

    StringNumber

---
+++ CSharpFundas/StringNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFundas
+{
+    public class StringNumber
+    {
+        static void Main(string[] args)
+        {
+            string str = "5";
+            try
+            {
+                int a = Convert.ToInt32(str);
+                Console.WriteLine("Success");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Exception");
+            }
+
+            int b = 100;
+            string str2 = b.ToString();
+            Console.WriteLine(str2);
+        }
+    }
+}
