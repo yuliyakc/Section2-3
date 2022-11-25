ed93df3883e5289df4e1f234f324485aeded1883
Yuliya Kozina <yuliyakc21@gmail.com>
2022-11-25 16:10:37 +0200

    Add Dictionary

---
+++ CSharpFundas/Dictionary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic; 
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CSharpFundas
+{
+    public class Dictionary
+    {
+        static void Main(string[] args)
+        {
+            List<string> lstMonths = new List<string>();
+            lstMonths.Add("January");
+            lstMonths.Add("February");
+            lstMonths.Add("March");
+
+            var February = lstMonths[1];
+            var March = lstMonths[2];
+
+            var months = new Dictionary<int, string>
+            {
+                {1, "January"},
+                {2, "February"},
+                {3, "March"}
+
+            };
+            months[4] = "April";
+            months.Add(5, "May");
+
+            Console.WriteLine(months); 
+
+        }
+    }
+}
