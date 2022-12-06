f9cdf2c27eab0744f5d77d7580ad468d44b83138
Yuliya Kozina <yuliyakc21@gmail.com>
2022-12-06 17:01:04 +0200

    toString, Equals

---
+++ CSharpFundas/Equals.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFundas
+{
+    public class Equals
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            int j = 10;
+            Console.WriteLine(i == j);
+            Console.WriteLine(i.Equals(j));
+        
+        }
+    }
+}
---
+++ CSharpFundas/toString.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFundas
+{
+    public class toString
+    {
+        static void Main(string[] args)
+        {
+
+            //toString() = converts an object to its string representation so that it is suitable for display
+
+            Car car = new Car("Chevy", "Corvette", 2022, "blue");
+            Console.WriteLine(car); 
+            Console.ReadKey();
+        }
+    }
+}
+class Car
+{
+    String make;
+    String model;
+    int year;
+    String color;
+
+    public Car(String make, String model, int year, String color)
+    { 
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+    
+    }
+
+    public override string ToString()
+    {
+        return "This is a " + make + " " + model;
+    }
+
+}
+
